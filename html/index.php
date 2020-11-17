50bcaa324d10806ff13bc435b3b20d1c1086b345
Денис75K <knnagrina36@mail.ru>
2020-11-18 00:54:42 +0300

    Уроки

---
+++ html/index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Gimn</title>
+</head>
+<body>
+  <h1>Гимн</h1>
+  <div>
+  <p>
+		  Россия — священная наша держава,</br>
+		  Россия — любимая наша страна.</br>
+		  Могучая воля, великая слава —</br>
+		  Твоё достоянье на все времена!</br>
+	</p>
+	
+	<p>
+	    Славься, Отечество наше свободное,</br>
+	    Братских народов союз вековой,</br>
+	    Предками данная мудрость народная!</br>
+	    Славься, страна! Мы гордимся тобой!</br>
+	</p>
+
+	<p>
+	     От южных морей до полярного края</br>
+	     Раскинулись наши леса и поля.</br>
+	     Одна ты на свете! Одна ты такая —</br>
+	     Хранимая Богом родная земля!</br>
+	</p>
+
+	<p>
+		  Славься, Отечество наше свободное,</br>
+	    Братских народов союз вековой,</br>
+	    Предками данная мудрость народная!</br>
+		  Славься, страна! Мы гордимся тобой!</br>
+	</p>
+
+	<p>
+		  Широкий простор для мечты и для жизни</br>
+		  Грядущие нам открывают года.</br>
+	    Нам силу даёт наша верность Отчизне.</br>
+		  Так было, так есть и так будет всегда!</br>
+	</p>
+
+	<p>
+	    Славься, Отечество наше свободное,</br>
+		  Братских народов союз вековой,</br>
+	    Предками данная мудрость народная!</br>
+		  Славься, страна! Мы гордимся тобой!</br>
+  </p>
+  </div>
+  <div>
+  <h1>МОЁ ПОРТФОЛИО</h1>
+  <div>
+    <h2 align="center">Обо мне</h2>
+    <img src="image/medved.jpg" alt="Это я" height="250">
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, provident, fuga, alias, modi doloremque et nemo vel doloribus tempore
+    expedita obcaecati impedit praesentium unde mollitia tempora cupiditate sed. Est, ratione, blanditiis, itaque magnam ea veniam adipisci ipsum
+    quam nesciunt fugit eligendi sint totam maxime dolor nisi numquam excepturi. Aperiam, ut, nam quibusdam aspernatur ad facere provident porro 
+    ratione itaque laudantium! Quisquam, itaque, dolore magni aliquam commodi cumque esse ex aperiam reprehenderit mollitia animi qui est atque
+    tempore amet eum quod! Facilis, sit laudantium aliquid sequi est eius rem dolore laborum asperiores. Nihil, et tempore aut voluptatum 
+    doloremque reprehenderit! Corrupti, iusto, animi, aspernatur modi numquam ullam corporis enim qui quaerat voluptatum iste saepe atque
+    impedit! Commodi doloribus veritatis labore. Magnam, corporis, vitae, minima, quibusdam obcaecati vel consectetur voluptas adipisci cumque
+    voluptatum sed dolorem incidunt corrupti porro molestiae voluptatem molestias nemo aspernatur illum debitis culpa? Sapiente, error nam 
+    reprehenderit nostrum eius ratione! 
+    </p>
+    <h2> Я в социальных сетях </h2>
+    <ol>
+      <li> <a href="https://ok.ru/" target="_blank">одноклассники</a></li>
+      <li> <a href="https://vk.com/" target="_blank">в контакте</a></li>
+      <li> <a href="https://ru-ru.facebook.com/" target="_blank"/>facebook</a></li>
+      <li> <a href="https://twitter.com/" target="_blank">твиттер</a></li>
+    </ol>
+  </div>
+    
+  </div>
+</body>
+</html>
