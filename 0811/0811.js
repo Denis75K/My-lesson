255682674113717b33a582d3c70bda1acd2b6454
Денис75K <knnagrina36@mail.ru>
2020-11-18 00:47:33 +0300

    уроки

---
+++ 0811/0811.js
@@ -0,0 +1,134 @@
+"use strict"
+
+// let cup ={}; 
+// cup = {
+//   color: "white",
+//   filled: false,
+//   liquid: null,
+//   handle: 1,
+//   material: "ceramic",
+//   cleanliness: 0.75,
+//   spoon: { material: "metal", cleanliness: 0.75},
+  
+//   fill: function(liquidTupe) {
+//     if (!cup.filled) {
+//       console.log("в кружку налили" + liquidTupe);
+//     } else {
+//       console.log("кружка уже наполнена");
+//     }
+//   },
+//   drink () {
+//     // if(cup.filled) {
+//     //   console.log("вы выпили" + cup.liquid);
+//     //   cup.filled = false;
+//     //   cup.liquid = null;
+//     if(this.filled) {
+//       console.log("вы выпили" + this.liquid);
+//       this.filled = false;
+//       this.liquid = null;
+//     } else {
+//       console.log("Нечего пить");
+//     }
+//   }
+// }
+// cup.fill("Чай");
+
+// числа
+
+// let a = 3.57;
+// let floor = Math.floor(a);
+// console.log(floor);
+// let round = Math.round(a);
+// console.los(round);
+// let ceil = Math.ceil(a);
+// console.log(ceil);
+
+// let fixed = a.toFixed(1);
+// console.log(fixed);
+
+// let width = "100px";
+// width = parseInt(width);
+
+
+// console.log(Math.random());
+
+// //let random =Math.floor(Math.random() * 10); // от 0 до 10
+// let random =10 + Math.floor(Math.random() * 10); // от 10 до 20
+
+// console.log(random);
+
+// // строки. методы строк.
+
+// let string = "что-то, что мы можем написать в одну строку";
+
+// let user = {name: "Василий", role: "Модератор"};
+// let badge = `
+// <div> 
+// <h2>Имя: ${user.name}</h2>
+// <h5>Роль: ${user.role}</h5>
+// </div>
+// `;
+
+// let str1 = "привет\nмир";
+
+// // эмодзи = \u(код)
+
+// // Длинна строки и доступ к символам
+
+// let str = "foobar";
+// console.log(str.length); // длинна строки;
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[3]);
+
+// let num = 325;
+// num = String(num);
+
+// console.log(num[0]);
+// console.log(num[1]);
+// console.log(num[2]);
+
+// let result ="";
+
+// for(let i=0; i < num.length; i++) {
+//   result = result + num[i]*10 + " ";
+// }
+
+// console.log(result);
+
+// изменение регистра
+
+let str1 ="Съешь ещё этих французких булок";
+let newStr1 = str1.toUpperCase();
+console.log(newStr1);
+let str2 = "Съешь ещё этих французких булок";
+let newStr2 = str2.toLowerCase();
+console.log(newStr2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
